951742a1bb1d97e55282998469d5313085b5cfb5
zerowk <em@zerowk.it>
2023-05-31 18:30:03 +0200

    fix(index): update import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
-import Title from "./components/Title/Title";
 import { Route, Routes } from "react-router-dom";
 import Character from "./page/Character";
 import Home from "./components/Home/Home";
