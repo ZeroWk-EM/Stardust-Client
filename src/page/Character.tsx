071d5fcf527237de2ea5bb94880560d61899e9f6
zerowk <em@zerowk.it>
2023-05-31 18:30:46 +0200

    feat(page): Add Character page

--- src/page/Character.tsx
+++ src/page/Character.tsx
@@ -1,9 +1,28 @@
-function Character() {
+import Card from "../components/Card/Card";
+import NewElement from "../components/NewElement/NewElement";
+import { useCharacter } from "../hook/useCharacter";
+
+const Character = () => {
+  let [character] = useCharacter({});
+
   return (
-    <div className="Character bg-dark">
-        <p>Character</p>
+    <div className="character bg-dark">
+      <div className="container mt-4">
+        <div className="row row-cols-1 row-cols-md-3 g-4">
+          <NewElement title={"Character"} />
+          {character &&
+            character.map((item) => {
+              return (
+                <Card
+                  image={String(item.image)}
+                  full_name={`${item.name} ${item.surname}`}
+                />
+              );
+            })}
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default Character;
