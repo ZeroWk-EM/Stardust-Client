a1df34053892ca49b96b16bb9956fea3db766440
wkasper <em@zerowk.it>
2023-06-01 12:21:10 +0200

    feat(footer): add footer component in app

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Character from "./page/Character";
 import Home from "./components/Home/Home";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route index path="/" element={<Home />} />
         <Route index path="/characters" element={<Character />} />
       </Routes>
+      <Footer/>
     </div>
   );
 }
