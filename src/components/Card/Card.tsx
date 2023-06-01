383d4e8060b41747022cde51e1c501dd9321766a
wkasper <em@zerowk.it>
2023-06-01 12:19:50 +0200

    fix(card): fix background size

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,12 +1,12 @@
+import "./Card.css";
 const Card = (props: { image: string; full_name: string }) => {
   return (
     <div className="col-md-3">
       <div
-        className="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"
+        className="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg sizig_img"
         style={{
           minHeight: "500px",
           background: `linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5)),url(${props.image})`,
-          backgroundSize: `cover`,
         }}
       >
         <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
