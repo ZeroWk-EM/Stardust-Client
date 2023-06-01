cec740808e18f8aeb029345f81a07af83f97e39c
wkasper <em@zerowk.it>
2023-06-01 12:20:16 +0200

    feat(useCharacter): update use character func

--- src/hook/useCharacter.ts
+++ src/hook/useCharacter.ts
@@ -3,17 +3,21 @@ import ICharacter from "../interface/characters.interface";
 import axios from "axios";
 const urlCharacter = "http://localhost:5000/v1/characters";
 
-export const useCharacter = (props: ICharacter): [ICharacter[]] => {
+export const useCharacter = (props: ICharacter, page = 1): [ICharacter[],Number,Number] => {
   const [character, setcharacter] = useState<ICharacter[]>([]);
+  const [maxpage, setMaxpage] = useState(0);
+  const [totalCharacter, settotalCharacter] = useState(0)
   useEffect(() => {
     axios
-      .get<ICharacter>(`${urlCharacter}`)
+      .get<ICharacter>(`${urlCharacter}?page=${page}`)
       .then((response: any) => {
         setcharacter(response.data.characters);
+        setMaxpage(response.data.info.maxpage);
+        settotalCharacter(response.data.info.totalCharacter);
       })
       .catch((err) => {
         console.log(err);
       });
-  }, []);
-  return [character];
+  }, [page]);
+  return [character,maxpage,totalCharacter];
 };
