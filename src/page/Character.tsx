8e95fddaba69d1d9a7ed83d094c1d038c49bccee
wkasper <em@zerowk.it>
2023-06-01 12:20:40 +0200

    feat(character): add character pagination

--- src/page/Character.tsx
+++ src/page/Character.tsx
@@ -1,27 +1,65 @@
+import { useState } from "react";
 import Card from "../components/Card/Card";
 import NewElement from "../components/NewElement/NewElement";
 import { useCharacter } from "../hook/useCharacter";
 
 const Character = () => {
-  let [character] = useCharacter({});
+  const [page, setPage] = useState<number>(1);
+  let [character, maxpage, totalCharacter] = useCharacter({}, page);
 
   return (
-    <div className="character bg-dark">
-      <div className="container mt-4">
-        <div className="row row-cols-1 row-cols-md-3 g-4">
-          <NewElement title={"Character"} />
-          {character &&
-            character.map((item) => {
+    <>
+      <div
+        className="btn-toolbar mt-4 d-flex justify-content-center align-items-center flex-column"
+        role="toolbar"
+        aria-label="Toolbar with button groups"
+      >
+        <div className="btn-group me-2" role="group" aria-label="First group">
+          <button
+            type="button"
+            className="btn btn-warning"
+            onClick={() => setPage(1)}
+          >
+            1
+          </button>
+          {Number(maxpage) >= 2 &&
+            Array.from({ length: Number(maxpage) - 1 }, (_, i) => {
               return (
-                <Card
-                  image={String(item.image)}
-                  full_name={`${item.name} ${item.surname}`}
-                />
+                <button
+                  type="button"
+                  className="btn btn-warning"
+                  onClick={() => {
+                    setPage(i + 2);
+                    console.log(i + 2);
+                  }}
+                  key={i}
+                >
+                  {i + 2}
+                </button>
               );
             })}
         </div>
       </div>
-    </div>
+      <div className="d-flex justify-content-center align-items-center mt-4">
+      <span className="badge text-bg-warning">Total Character {Number(totalCharacter)}</span>
+      </div>
+      <div className="character bg-dark">
+        <div className="container mt-4">
+          <div className="row row-cols-1 row-cols-md-3 g-4">
+            {page === 1 ? <NewElement title={"Character"} /> : <></>}
+            {character &&
+              character.map((item) => {
+                return (
+                  <Card
+                    image={String(item.image)}
+                    full_name={`${item.name} ${item.surname}`}
+                  />
+                );
+              })}
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
