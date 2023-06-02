6bc9a3eb20bb610b11a8d26588894b0bb94dbd42
zerowk <em@zerowk.it>
2023-06-02 19:06:35 +0200

    feat(modal): all add modal end-point

--- src/components/modal/characterModal.tsx
+++ src/components/modal/characterModal.tsx
@@ -12,7 +12,7 @@ const CharacterModal = (props: { action: string }) => {
       aria-labelledby="exampleModalLabel"
       aria-hidden="true"
     >
-      <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+      <div className="modal-dialog modal-dialog-centered modal-xl">
         <div className="modal-content">
           <div className="modal-header">
             <h1 className="modal-title fs-5" id="exampleModalLabel">
