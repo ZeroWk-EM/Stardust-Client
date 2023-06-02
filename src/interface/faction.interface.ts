cd76ae73b622a26610fa03a727100d4d73226fac
zerowk <em@zerowk.it>
2023-06-02 15:28:17 +0200

    feat(interface): crearte movie interface

---
+++ src/interface/faction.interface.ts
@@ -0,0 +1,10 @@
+interface IFaction {
+    image?: string;
+    name?: string;
+    description?: string;
+    planet?: string[];
+    weapons?: string[];
+    technology?: string[];
+  }
+  
+  export default IFaction;
