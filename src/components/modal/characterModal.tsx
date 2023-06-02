845f1453afcd750fd597830c54f05dbd143dcf42
zerowk <em@zerowk.it>
2023-06-02 15:27:54 +0200

    fix(modal): update import

--- src/components/modal/characterModal.tsx
+++ src/components/modal/characterModal.tsx
@@ -1,7 +1,9 @@
 import "./modal.css";
-import { useFilm } from "../../hook/useFilm";
+import { useMovie } from "../../hook/useMovie";
+import { useFaction } from "../../hook/useFaction";
 const CharacterModal = (props: { action: string }) => {
-  let [film] = useFilm({});
+  let [film] = useMovie({});
+  let [faction] = useFaction({});
   return (
     <div
       className="modal fade"
@@ -10,7 +12,7 @@ const CharacterModal = (props: { action: string }) => {
       aria-labelledby="exampleModalLabel"
       aria-hidden="true"
     >
-      <div className="modal-dialog modal-dialog-centered">
+      <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
         <div className="modal-content">
           <div className="modal-header">
             <h1 className="modal-title fs-5" id="exampleModalLabel">
@@ -25,17 +27,16 @@ const CharacterModal = (props: { action: string }) => {
           </div>
           <div className="modal-body">
             <form className="row g-3">
-
-            <div className="col-12">
+              <div className="col-12">
                 <div className="form-floating mb-3">
                   <input
                     type="text"
                     className="form-control rounded-3"
-                    id="floatingInput"
+                    id="urlInput"
                     placeholder="Insert url"
-
+                    required
                   />
-                  <label htmlFor="floatingInput">Insert image URL</label>
+                  <label htmlFor="urlInput">Insert image URL</label>
                 </div>
               </div>
 
@@ -44,10 +45,11 @@ const CharacterModal = (props: { action: string }) => {
                   <input
                     type="text"
                     className="form-control rounded-3"
-                    id="floatingInput"
+                    id="nameInput"
                     placeholder="Insert name"
+                    required
                   />
-                  <label htmlFor="floatingInput">Insert character name</label>
+                  <label htmlFor="nameInput">Insert character name</label>
                 </div>
               </div>
               <div className="col-md-6">
@@ -55,10 +57,10 @@ const CharacterModal = (props: { action: string }) => {
                   <input
                     type="text"
                     className="form-control rounded-3"
-                    id="floatingInput"
+                    id="surnameInput"
                     placeholder="name@example.com"
                   />
-                  <label htmlFor="floatingInput">Insert character surname</label>
+                  <label htmlFor="surnameInput">Insert character surname</label>
                 </div>
               </div>
 
@@ -69,6 +71,7 @@ const CharacterModal = (props: { action: string }) => {
                     className="form-control rounded-3"
                     id="weightInput"
                     placeholder="Insert weight"
+                    required
                   />
                   <label htmlFor="weightInput">Insert weight</label>
                 </div>
@@ -81,42 +84,21 @@ const CharacterModal = (props: { action: string }) => {
                     className="form-control rounded-3"
                     id="heightInput"
                     placeholder="Insert height"
+                    required
                   />
                   <label htmlFor="heightInput">Insert height</label>
                 </div>
               </div>
 
-              <div className="col-12">
-                <select className="form-select form-select-lg" aria-label="Default select example">
-                    <option selected>Select gender</option>
-                    <option value="Male">Male</option>
-                    <option value="Female">Female</option>
-                    <option value="Asexual">Asexual</option>
-                    <option value="Unknown">Unknown</option>
-                    <option value="Other">Other</option>
-                </select>
-              </div>
-
-              <div className="col-12">
-                <select className="form-select form-select-lg" aria-label="Default select example">
-                <option selected>Select film</option>
-                {film && film.map((item)=>{
-                  return (
-                    <option value={item.title}>{item.title}</option>
-                  )
-                })}
-                </select>
-              </div>
-
               <div className="col-md-6">
                 <div className="form-floating mb-3">
                   <input
                     type="text"
                     className="form-control rounded-3"
-                    id="heightInput"
+                    id="hairInput"
                     placeholder="Insert hair color"
                   />
-                  <label htmlFor="heightInput">Insert hair color</label>
+                  <label htmlFor="hairInput">Insert hair color</label>
                 </div>
               </div>
 
@@ -125,14 +107,142 @@ const CharacterModal = (props: { action: string }) => {
                   <input
                     type="text"
                     className="form-control rounded-3"
-                    id="heightInput"
+                    id="skinInput"
                     placeholder="Insert skin color"
+                    required
                   />
-                  <label htmlFor="heightInput">Insert skin color</label>
+                  <label htmlFor="skinInput">Insert skin color</label>
                 </div>
               </div>
 
+              <div className="col-6">
+                <select
+                  className="form-select form-select-lg"
+                  aria-label="Default select example"
+                  required
+                >
+                  <option selected>Select gender</option>
+                  <option value="Male">Male</option>
+                  <option value="Female">Female</option>
+                  <option value="Asexual">Asexual</option>
+                  <option value="Unknown">Unknown</option>
+                  <option value="Other">Other</option>
+                </select>
+              </div>
 
+              <div className="col-6">
+                <select
+                  className="form-select form-select-lg"
+                  aria-label="Default select example"
+                  required
+                >
+                  <option selected>Select eye color</option>
+                  <option value="Brown">Brown</option>
+                  <option value="Amber">Amber</option>
+                  <option value="Hazel">Hazel</option>
+                  <option value="Green">Green</option>
+                  <option value="Blue">Blue</option>
+                  <option value="Gray">Gray</option>
+                  <option value="Yellow">Yellow</option>
+                  <option value="Red">Red</option>
+                  <option value="Unknown">Unknown</option>
+                </select>
+              </div>
+
+              <div className="col-6">
+                <select
+                  className="form-select form-select-lg"
+                  aria-label="Default select example"
+                >
+                  <option selected>Select homeworld</option>
+
+                </select>
+              </div>
+
+              <div className="col-6">
+                <select
+                  className="form-select form-select-lg"
+                  aria-label="Default select example"
+                  required
+                >
+                  <option selected>Select specie</option>
+         
+                </select>
+              </div>
+
+              <div className="col-md-7">
+                <div className="form-floating mb-3">
+                  <input
+                    type="number"
+                    className="form-control rounded-3"
+                    id="birthInput"
+                    placeholder="Insert height"
+                  />
+                  <label htmlFor="birthInput">Insert birth year</label>
+                </div>
+              </div>
+
+              <div className="col-5">
+                <select
+                  className="form-select form-select-lg"
+                  aria-label="Default select example"
+                  style={{ minHeight: "57px" }}
+                >
+                  <option selected>Select period</option>
+                  <option value="ABY">ABY</option>
+                  <option value="BBY">BBY</option>
+                </select>
+              </div>
+
+              <div className="col-6">
+                <select className="form-select form-select-lg">
+                  <option selected>Select film</option>
+                  {film &&
+                    film.map((item) => {
+                      return <option value={item.title}>{item.title}</option>;
+                    })}
+                </select>
+              </div>
+
+              <div className="col-6">
+                <select
+                  className="form-select form-select-lg"
+                  aria-label="Default select example"
+                >
+                  <option selected>Select series</option>
+                  
+                </select>
+              </div>
+              <div className="col-12">
+                <select
+                  className="form-select form-select-lg"
+                  aria-label="Default select example"
+                >
+                  <option selected>Select Faction</option>
+                  {faction &&
+                    faction.map((item) => {
+                      return <option value={item.name}>{item.name}</option>;
+                    })}
+                </select>
+              </div>
+              <div className="col-6">
+                <select
+                  className="form-select form-select-lg"
+                  aria-label="Default select example"
+                >
+                  <option selected>Select Vehicles</option>
+           
+                </select>
+              </div>
+              <div className="col-6">
+                <select
+                  className="form-select form-select-lg"
+                  aria-label="Default select example"
+                >
+                  <option selected>Select Weapons</option>
+              
+                </select>
+              </div>
             </form>
           </div>
         </div>
@@ -141,5 +251,4 @@ const CharacterModal = (props: { action: string }) => {
   );
 };
 
-
-export default CharacterModal
+export default CharacterModal;
