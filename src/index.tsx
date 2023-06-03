f6635357245dff1db9a0b0642c229991f937a011
zerowk <em@zerowk.it>
2023-06-03 20:42:01 +0200

    feat(index): add primereact import

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,9 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
-
+import "primereact/resources/themes/lara-light-indigo/theme.css";     
+import "primereact/resources/primereact.min.css";                                       
+        
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
