f45cfea40c1cdd232608301704550cd9aeb9f3ed
zerowk <em@zerowk.it>
2023-06-04 12:15:42 +0200

    feat(hook): add try a cath

--- src/hook/useCharacter.ts
+++ src/hook/useCharacter.ts
@@ -3,21 +3,28 @@ import ICharacter from "../interface/characters.interface";
 import axios from "axios";
 const urlCharacter = "http://localhost:5000/v1/characters";
 
-export const useCharacter = (props: ICharacter, page = 1): [ICharacter[],Number,Number] => {
+export const useCharacter = (
+  props: ICharacter,
+  page = 1
+): [ICharacter[], Number, Number] => {
   const [character, setcharacter] = useState<ICharacter[]>([]);
   const [maxpage, setMaxpage] = useState(0);
-  const [totalCharacter, settotalCharacter] = useState(0)
+  const [totalCharacter, settotalCharacter] = useState(0);
   useEffect(() => {
-    axios
-      .get<ICharacter>(`${urlCharacter}?page=${page}`)
-      .then((response: any) => {
-        setcharacter(response.data.characters);
-        setMaxpage(response.data.info.maxpage);
-        settotalCharacter(response.data.info.totalCharacter);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    try {
+      axios
+        .get<ICharacter>(`${urlCharacter}?page=${page}`)
+        .then((response: any) => {
+          setcharacter(response.data.characters);
+          setMaxpage(response.data.info.maxpage);
+          settotalCharacter(response.data.info.totalCharacter);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } catch (e) {
+      console.log(e);
+    }
   }, [page]);
-  return [character,maxpage,totalCharacter];
+  return [character, maxpage, totalCharacter];
 };
