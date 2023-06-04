3c5fa951ecf69f5e8fe237ffa2b4272cf63d36da
zerowk <em@zerowk.it>
2023-06-04 12:16:01 +0200

    feat(ErrorPage): define error page

---
+++ src/page/ErrorPage.tsx
@@ -0,0 +1,17 @@
+const ErroPage = (props:{message:string}) => {
+ 
+  return (
+    <div className="container">
+      <div className="row my-5">
+        <div className="card py-5">
+          <div className="title">
+            <h1 className="fs-3 mb-5 text-center">ERROR PAGE</h1>
+          </div>
+   <p className="text-center">{props.message}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ErroPage;
