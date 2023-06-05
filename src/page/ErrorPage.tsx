cb52cfeb40a12ad295f76aa6c618094bf349d8a0
wkasper <em@zerowk.it>
2023-06-05 15:34:20 +0200

    feat(page): update error page

--- src/page/ErrorPage.tsx
+++ src/page/ErrorPage.tsx
@@ -1,11 +1,11 @@
-const ErrorPage = (props:{message:string}) => {
+const ErrorPage = (props:{code?:number, message:string}) => {
  
   return (
     <div className="container">
       <div className="row my-5">
         <div className="card py-5">
           <div className="title">
-            <h1 className="fs-3 mb-5 text-center">ERROR PAGE</h1>
+            <h1 className="fs-3 mb-5 text-center">ERROR {props.code ? props.code: "PAGE"}</h1>
           </div>
    <p className="text-center">{props.message}</p>
         </div>
